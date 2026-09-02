46379005054f2fdf33471f1dd91cf7684a88104a
Giorgi <giorguna.jr@gmail.com>
2026-09-03 02:15:04 +0400

    Removed sleep resemble response await.

--- src/api/rooms-api.ts
+++ src/api/rooms-api.ts
@@ -1,10 +1,6 @@
 import type { Room } from "../modules/rooms/room-types";
 import { http } from "../utility/http-client";
 
-// Sleep to resemble response await.
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
 export const getRooms = async (signal?: AbortSignal): Promise<Room[]> => {
-    await sleep(1000);
     return await http.get("/rooms.json", undefined, { signal });
 };
